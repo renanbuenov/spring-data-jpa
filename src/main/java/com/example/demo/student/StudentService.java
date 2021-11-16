7711cf54a335d85bc23495b8edb3bca3cce94c3a
Renan Bueno <renanbuenov@hotmail.com>
2021-11-16 10:49:43 -0300

    Repository class created

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,20 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Autowired
     public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Renan",
-                "renanbueno@hotmail.com", LocalDate.of(1994, Month.DECEMBER,
-                24),
-                21));
+        return studentRepository.findAll();
     }
 }
